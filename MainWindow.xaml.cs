32649567a6c3c521ccd0fc5ad619e698829e177d
Leo <leodgrim117@gmail.com>
2023-07-24 22:38:02 -0500

    Added Open and Save As functionality. Encoding combobox for FileDialog classes will need to implemented later.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace CSharpNotepad
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace CSharpNotepad
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +12,27 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Functionality for Open menu button
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Open file explorer when button is pressed
+            //Set filter so user only sees txt files unless otherwise specified
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Documents (*.txt)|*.txt|All files (*.*)|*.*";
+            if(openFileDialog.ShowDialog() == true)
+                TextMain.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        //Functionality for Save As menu button
+        private void SaveAsButton_click(object sender, RoutedEventArgs e)
+        {
+            //Write all text to created file when user clicks SAVE
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Documents (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, TextMain.Text);
         }
     }
 }
